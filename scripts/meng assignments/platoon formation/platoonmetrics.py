dd25fa0abfcf829e3b8c079f161afcecc6c771ad
ronan-keane <rlk268@cornell.edu>
2019-11-10 18:04:09 -0500

    Update platoonmetrics.py

--- scripts/meng assignments/platoon formation/platoonmetrics.py
+++ scripts/meng assignments/platoon formation/platoonmetrics.py
@@ -21,4 +21,21 @@ unused, unused, laneplatoons = makeplatoonlist(data,n=5,lane=2,vehs=[582,1146])
 #platoon formation based on sorting    
 unused, unused, sortedplatoons = makeplatoonlist_s(data,n=5,lane=6)
     
+#%%
+#note that havsim.calibration.helper.makeleadfolinfo can be used to get the leaders 
+#for a platoon, which may be convenient. 
 
+from havsim.calibration.helper import makeleadfolinfo
+
+testplatoon = [[], 381.0, 391.0, 335.0, 326.0, 334.0]
+leadinfo, folinfo, unused = makeleadfolinfo(testplatoon, platooninfo, meas)
+
+#leadinfo[2] = [[316.0, 1302, 1616], [318.0, 1617, 1644]]
+#this means second vehicle in the platoon (testplatoon[1], which is 391)
+#follows vehicle 316 from time 1302 to 1616, and it follows 318 from 1617 to 1644. 
+
+"""
+TO DO 
+Implement functions which calculate the metrics for a given platoon
+Calculate manually what the chain metric should be for the platoon [[], 391, 335, 326] for k = 1 and k = 0. Show your work. 
+"""
