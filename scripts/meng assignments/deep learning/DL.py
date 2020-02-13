f6c7828d34216c43b6d24113f00c62c713ebf15f
ronan-keane <rlk268@cornell.edu>
2020-02-12 23:14:59 -0500

    Update DL.py

--- scripts/meng assignments/deep learning/DL.py
+++ scripts/meng assignments/deep learning/DL.py
@@ -45,7 +45,7 @@ statemem = 5
 #xtest, ytest = np.zeros((0,statemem*3)), np.zeros((0,1))
 xtrain, ytrain, xtest, ytest = [], [], [], []
 for count, i in enumerate(meas.keys()):
-    if count > 10:
+    if count > 200: # only use 200 vehicles 
         break
     t_nstar, t_n, T_nm1, T_n = platooninfo[i][:4]
     leadinfo, unused, unused = havsim.calibration.helper.makeleadfolinfo([i], platooninfo, meas)
@@ -78,22 +78,7 @@ for count, i in enumerate(meas.keys()):
             leadv = lead[j+1-statemem:j+1,1]
             vehv = curmeas[j+1-statemem:j+1,1]
             hd = headway[j+1-statemem:j+1]
-        
-#        curx = np.zeros((1,3*statemem))
-#        cury = np.zeros((1,1))
-#        curx[0,:statemem] = leadv
-#        curx[0,statemem:2*statemem] = vehv
-#        curx[0,2*statemem:3*statemem] = hd
-#        cury[0,0] = curmeas[j,2]
-#        
-#        #using np.append is extremely slow because you have to copy the entire array each call. 
-#        #would be faster if you used regular python lists, but there are problems with converting things into tensors
-#        if train_or_test[count]:
-#            xtrain = np.append(xtrain, curx,axis=0)
-#            ytrain = np.append(ytrain, cury,axis=0)
-#        else:
-#            xtest = np.append(xtest, curx,axis=0)
-#            ytest = np.append(ytest, cury,axis=0)
+
         curx = list(leadv)
         curx.extend(list(vehv))
         curx.extend(list(hd))
@@ -117,7 +102,7 @@ xtrain[:,statemem*2:statemem*3] = xtrain[:,statemem*2:statemem*3]/maxheadway
 xtest[:,:statemem*2] = xtest[:,:statemem*2]/maxvelocity
 xtest[:,statemem*2:statemem*3] = xtest[:,statemem*2:statemem*3]/maxheadway
     
-#%%  #you'll probably want to save the train_or_test, xtrain, ... ytest in a pickle
+ #you'll probably want to save the train_or_test, xtrain, ... ytest in a pickle
 
 xtrain, ytrain, xtest, ytest = tf.convert_to_tensor(xtrain,tf.float32), tf.convert_to_tensor(ytrain,tf.float32), tf.convert_to_tensor(xtest,tf.float32), tf.convert_to_tensor(ytest,tf.float32)
 
@@ -126,15 +111,19 @@ train_ds = tf.data.Dataset.from_tensor_slices(
 
 test_ds = tf.data.Dataset.from_tensor_slices(
         (xtest,ytest)).shuffle(100000).batch(32)
-
+#%%
 class Model(tf.keras.Model):
     def __init__(self):
         super().__init__('simple_mlp')
-        self.hidden1 = kls.Dense(32, activation = 'relu')
-        self.out = kls.Dense(1, activation = 'tanh')
+        self.hidden1 = kls.Dense(8, activation = 'relu')
+        self.hidden2 = kls.Dense(8,activation = 'relu')
+        self.hidden3 = kls.Dense(8)
+        self.out = kls.Dense(1)
     
     def call(self,x):
         x = self.hidden1(x)
+        x = self.hidden2(x)
+        x = self.hidden3(x)
         return self.out(x)
 
 model = Model()
@@ -142,42 +131,43 @@ model = Model()
 #%% Set up training 
 #x = input, y = output, yhat = labels (true values)
 
-optimizer = tf.keras.optimizers.RMSprop(learning_rate=2e-3)
+optimizer = tf.keras.optimizers.RMSprop(learning_rate=2e-5)
 
-loss_fn = tf.keras.metrics.MeanSquaredError(name='train_test_loss')
+loss_fn = tf.keras.losses.MeanSquaredError(name='train_test_loss')
 def mytestmetric(y,yhat):
     return tf.math.reduce_mean((y - yhat)**2)
 
 #note: can use model.fit and model.evaluate instead for this simple case 
 
 @tf.function
-def train_step(x,yhat):
-    with tf.GradienTape() as tape: 
+def train_step(x,yhat, loss_fn, optimizer):
+    with tf.GradientTape() as tape: 
         y = model(x)
         loss = loss_fn(y,yhat)
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients,model.trainable_variables))
     
-@tf.function
-def test_step(x,yhat):
-    y = model(x)
-    m = mytestmetric(y,yhat)
-    return m 
+#@tf.function
+def test(dataset, minacc, maxacc):
+    mse = []
+    for x, yhat in dataset:
+        y = model(x)
+        m = mytestmetric(y,yhat)
+        mse.append(m)
+    return (tf.math.reduce_mean(mse)**.5)*(maxacc-minacc)-minacc
     
-#%% test before training 
-
-for x, yhat in test_ds: 
-    m = test_step(x,yhat)
-
     
-
-#%%    
+#%% training and testing 
+m = test(test_ds,minacc,maxacc)
+m2 = test(train_ds, minacc,maxacc)
+print('before training rmse on test dataset is '+str(tf.cast(m,tf.float32))+' rmse on train dataset is '+str(m2))
 
 for epoch in range(5): 
     for x, yhat in train_ds:
-        train_step(x,yhat)
-    for x, yhat in test_ds:
-        test_step()
+        train_step(x,yhat,loss_fn, optimizer)
+    m = test(test_ds,minacc,maxacc)
+    m2 = test(train_ds, minacc,maxacc)
+    print('epoch '+str(epoch)+' rmse on test dataset is '+str(m)+' rmse on train dataset is '+str(m2))
     
 
     
