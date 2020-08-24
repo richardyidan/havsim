36d5296aa4f6be789338d19871357cebdae480c2
ronan-keane <rlk268@cornell.edu>
2020-08-24 19:33:03 -0400

    call for RNN maybe

--- scripts/meng assignments/deep learning/DL2.py
+++ scripts/meng assignments/deep learning/DL2.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import numpy as np
 from havsim.plotting import compute_headway2
 
-def make_dataset(meas, platooninfo, h = .1):
+def make_dataset(meas, platooninfo, dt = .1):
     # select training/testing vehicles
     nolc_list = []
     for veh in meas.keys():
@@ -21,7 +21,7 @@ def make_dataset(meas, platooninfo, h = .1):
     maxheadway, maxspeed = 0, 0
     minacc, maxacc = 1e4, -1e4
     for veh in train_veh:
-        headway = compute_headway2(veh, meas, platooninfo, h = h)
+        headway = compute_headway2(veh, meas, platooninfo, h = dt)
         t0, t1, t2, t3 = platooninfo[veh][:4]
         lead = platooninfo[veh][4][0]
         lt0, lt1, lt2, lt3 = platooninfo[lead][:4]
@@ -39,7 +39,7 @@ def make_dataset(meas, platooninfo, h = .1):
 
     testing = {}
     for veh in test_veh:
-        headway = compute_headway2(veh, meas, platooninfo, h = h)
+        headway = compute_headway2(veh, meas, platooninfo, h = dt)
         t0, t1, t2, t3 = platooninfo[veh][:4]
         lead = platooninfo[veh][4][0]
         lt0, lt1, lt2, lt3 = platooninfo[lead][:4]
@@ -55,20 +55,50 @@ def make_dataset(meas, platooninfo, h = .1):
 
 
 class RNNCFModel(tf.keras.Model):
-    def __init__(self, learning_rate = .001):
+    def __init__(self, maxhd, maxv, mina, maxa, learning_rate = .001, dt = .1):
         super().__init__()
+        # architecture
         self.lstm_cell = tf.keras.layers.LSTMCell(20)
         self.dense1 = tf.keras.layers.Dense(1)
 
+        # normalization constants
+        self.maxhd = maxhd
+        self.maxv = maxv
+        self.mina = mina
+        self.maxa = maxa
+        self.dt = dt
+
+        # optimizer and loss function
         self.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = learning_rate),
                      loss = tf.keras.losses.MeanSquaredError())
 
-    def call(self, x):
-        lead_inputs, init_state, hidden_states = x
-        cur_lead_inputs = tf.unstack(lead_inputs, axis=)
-        x = self.lstm_cell(x)
-        x = self.dense1(x)
-        return x
+    def call(self, inputs):
+        # prepare data for call
+        lead_inputs, init_state, hidden_states = inputs
+        lead_inputs = tf.unstack(lead_inputs, axis=1)  # unpacked over time dimension
+        cur_pos, cur_speed = tf.unstack(init_state, axis=1)
+        outputs = []
+        for cur_lead_input in lead_inputs:
+            # normalize data for current timestep
+            cur_lead_pos, cur_lead_speed = tf.unstack(cur_lead_input, axis=1)
+            curhd = cur_lead_pos-cur_pos
+            curhd = curhd/self.maxhd
+            cur_lead_speed = cur_lead_speed/self.maxv
+            norm_veh_speed = cur_speed/self.maxv
+            cur_inputs = tf.stack([curhd, norm_veh_speed, cur_lead_speed],axis=1)
+
+            # call to model
+            x, hidden_states = self.lstm_cell(cur_inputs, hidden_states)
+            x = self.dense1(x)  # output of the model is current acceleration for the batch
+
+            # update vehicle states
+            cur_acc = (self.maxa-self.mina)*x + self.mina
+            cur_speed = cur_speed + self.dt*cur_acc
+            cur_pos = cur_pos + self.dt*cur_acc
+            outputs.append(cur_pos)
+
+        outputs = tf.concat(outputs, -1)
+        return outputs
 
 def make_batch(ds, nveh = 20, nt = 5, lstm_units = 20):
     """Create batch of data to send to model.
