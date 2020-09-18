8d2f4f41f673f6efe72080829879fa4b4a09f533
ronan-keane <rlk268@cornell.edu>
2020-09-18 11:37:38 -0400

    figures update

--- scripts/spring 2020/relax results/make_figure.py
+++ scripts/spring 2020/relax results/make_figure.py
@@ -9,6 +9,8 @@ import numpy as np
 import copy
 from havsim import calibration
 from havsim.calibration import calibration_models
+import tensorflow as tf
+import pickle
 #%%  # plot a platoon of vehicles
 vehs = [1145, 1152, 1155, 1163, 1173, 1174, 1183, 1177, 1184, 1193, 1200, 1214, 1215, 1219,
         1226, 1232, 1241, 1240, 1243, 1252, 1260, 1266, 1267, 1275, 1278, 1282, 1283, 1293, 1290,
@@ -107,6 +109,7 @@ plt.xlabel('time')
 #%%  need the output from /relax results/NN.py 
 # _r -> the output from DL2_relax which was calibrated with relaxation on all vehicles = trained LSTM with relax 
 # _nor -> the output from DL2 which was calibrated on vehicles with no lane changing = trained LSTM
+# need to run sections 0, 3, 4 in NN.py in scripts/spring 2020/relax results
 
 with open('RNNCFtraj.pkl', 'rb') as f:
     nolc_r_res, out3, lc_r_res, out4 = pickle.load(f)
@@ -126,7 +129,7 @@ plt.close('all')
 sim_rnn_nor = copy.deepcopy(meas)
 sim_rnn = copy.deepcopy(meas)
 
-veh_to_plot = [793]
+veh_to_plot = [1977]
 try:
     for veh in veh_to_plot:
         t0, t1, t2, t3 = platooninfo[veh][:4]
@@ -148,8 +151,8 @@ except:
 # plt.legend([C1[0], C0[0]], ['LSTM with relax','data'])
 t = np.linspace(t1, t2, t2+1-t1)
 plt.figure()
-plt.plot(t, meas[veh][t1-t0:t2-t0+1,3], t, sim_rnn_nor[veh][t1-t0:t2-t0+1,3])
-# plt.legend(['data', 'LSTM no relax', 'LSTM with relax'])
+plt.plot(t, meas[veh][t1-t0:t2-t0+1,3], t, sim_rnn_nor[veh][t1-t0:t2-t0+1,3], t, sim_rnn[veh][t1-t0:t2-t0+1,3] )
+plt.legend(['data', 'LSTM no relax', 'LSTM with relax'])
 plt.title('speed-time for vehicle '+str(int(veh)))
 plt.ylabel('speed (ft/s)')
 plt.xlabel('time index (.1s)')
@@ -187,7 +190,7 @@ kwargs_ska = {'vehicle_class': calibration_models.SKA_IDM}
 
 
 #%%
-veh_to_plot = [793]
+veh_to_plot = [1977]
 veh = veh_to_plot[0]
 t0, t1, t2, t3 = platooninfo[veh][:4]
 sim_idm = copy.deepcopy(meas)
@@ -259,23 +262,23 @@ else:
 # hp.plotvhd(meas, sim_nn, platooninfo, veh_to_plot, draw_arrow=True)
 # plt.legend([C1[0], C0[0]], ['NM with relax', 'data'])
     
-# t = np.linspace(t1, t2, t2+1-t1)
-# plt.figure()
-# plt.plot(t, meas[veh][t1-t0:t2-t0+1,3], t, sim_idm_nor[veh][t1-t0:t2-t0+1,3], t, sim_idm[veh][t1-t0:t2-t0+1,3], t, sim_ska[veh][t1-t0:t2-t0+1,3])
-# plt.legend(['data', 'IDM no relax', 'IDM with relax', 'IDM-SKA'])
-# plt.title('speed-time for vehicle '+str(int(veh)))
-# plt.ylabel('speed (ft/s)')
-# plt.xlabel('time index (.1s)')
+t = np.linspace(t1, t2, t2+1-t1)
+plt.figure()
+plt.plot(t, meas[veh][t1-t0:t2-t0+1,3], t, sim_idm_nor[veh][t1-t0:t2-t0+1,3], t, sim_idm[veh][t1-t0:t2-t0+1,3], t, sim_ska[veh][t1-t0:t2-t0+1,3])
+plt.legend(['data', 'IDM no relax', 'IDM with relax', 'IDM-SKA'])
+plt.title('speed-time for vehicle '+str(int(veh)))
+plt.ylabel('speed (ft/s)')
+plt.xlabel('time index (.1s)')
 
-# plt.figure()
-# plt.plot(t, meas[veh][t1-t0:t2-t0+1,3], t, sim_nn_nor[veh][t1-t0:t2-t0+1,3], t, sim_nn[veh][t1-t0:t2-t0+1,3], t, sim_ll[veh][t1-t0:t2-t0+1,3])
-# plt.legend(['data', 'NM no relax', 'NM with relax', 'NM-LL'])
-# plt.title('speed-time for vehicle '+str(int(veh)))
-# plt.ylabel('speed (ft/s)')
-# plt.xlabel('time index (.1s)')
+plt.figure()
+plt.plot(t, meas[veh][t1-t0:t2-t0+1,3], t, sim_nn_nor[veh][t1-t0:t2-t0+1,3], t, sim_nn[veh][t1-t0:t2-t0+1,3], t, sim_ll[veh][t1-t0:t2-t0+1,3])
+plt.legend(['data', 'NM no relax', 'NM with relax', 'NM-LL'])
+plt.title('speed-time for vehicle '+str(int(veh)))
+plt.ylabel('speed (ft/s)')
+plt.xlabel('time index (.1s)')
 
-plt.plot(t, sim_idm[veh][t1-t0:t2-t0+1,3], t, sim_nn[veh][t1-t0:t2-t0+1,3])
-plt.legend(['data', 'LSTM no relax', 'IDM no relax', 'NM no relax'])
+# plt.plot(t, sim_idm[veh][t1-t0:t2-t0+1,3], t, sim_nn[veh][t1-t0:t2-t0+1,3])
+# plt.legend(['data', 'LSTM no relax', 'IDM no relax', 'NM no relax'])
 
 
 
